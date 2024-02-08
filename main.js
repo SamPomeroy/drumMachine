3c2c4bc2aea0598c5719cef2436c6c88ebc9a518
Samantha Pomeroy <samanthapomeroy38@gmail.com>
2024-02-07 18:22:22 -0600

    line 32

--- main.js
+++ main.js
@@ -1,20 +1,54 @@
 // Setup 'tick' sound
-const tick = new Audio('sounds/tick.mp3');
-const tock = new Audio('sounds/tock.mp3');
-let count = 0
-const counter = document.querySelector('#counter')
+const tickSound = new Audio('sounds/tick.mp3');
+const tockSound = new Audio('sounds/tock.mp3');
+const hiHatSound = new Audio('sounds/hi-hat.mp3');
+const kickDrumSound = new Audio('sounds/kick-drum.mp3');
+const snareDrumSound = new Audio('sounds/snare-drum.mp3')
+let count = 0;
+const counter = document.querySelector('#counter');
+const metronome = document.querySelector('#metronome');
+const hiHat = document.querySelector('#hiHat');
+const kickDrum = document.querySelector('#kickDrum')
+const snareDrum = document.querySelector('#snareDrum')
 
 // This function is called every 600ms
 function update() {
-if(count % 4 === 0){
-    tock.play();
-    
-}else{
-    // Play the 'tick' sound
-    tick.play();
+    if(metronome.checked){
+        if((count+2)%4 === 0){
+            tockSound.play();
+        }else{
+            // Play the 'tick' sound
+            tickSound.play();
+            }
     }
+    if(hiHat.checked){
+        // if((count+2)%4 === 0){
+        //     tockSound.play();
+        // }else{
+            // Play the 'tick' sound
+            hiHatSound.play();
+            }
+    // }
+    if(kickDrum.checked && document.querySelector('#kickDrumInterval').value == count){
+        // if((count+2)%4 === 0){
+        //     tockSound.play();
+        // }else{
+            // Play the 'tick' sound
+            kickDrumSound.play();
+            }
+    // }
+    if(snareDrum.checked){
+        // if((count+2)%4 === 0){
+        //     tockSound.play();
+        // }else{
+            // Play the 'tick' sound
+            snareDrumSound.play();
+            }
+    // }
     count++
-    counter.innerText = `${count % 4}`
+    counter.innerText = `${count % 4 +1}`
+   
+    
 
 }
 
